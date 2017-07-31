9164038580cf05d3d79c3052dbdec5d601acd78f
alun <alunfeixue@qq.com>
2017-07-31 11:05:09 +0800

    commit

---
+++ library/src/main/java/com/daohen/thirdparty/library/glide/DaohenAppGlideModule.java
@@ -0,0 +1,17 @@
+package com.daohen.thirdparty.library.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * CREATE BY ALUN
+ * EMAIL: alunfeixue2011@gmail.com
+ * DATE : 2017/07/31 10:10
+ */
+
+@GlideModule
+public class DaohenAppGlideModule extends AppGlideModule {
+
+    
+
+}
