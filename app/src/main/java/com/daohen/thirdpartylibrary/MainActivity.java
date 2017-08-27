5d25ae6472a34d434d6ea75925454f53c6294524
daohen <alunfeixue@qq.com>
2017-08-27 16:15:19 +0800

    commit

--- app/src/main/java/com/daohen/thirdpartylibrary/MainActivity.java
+++ app/src/main/java/com/daohen/thirdpartylibrary/MainActivity.java
@@ -2,20 +2,13 @@ package com.daohen.thirdpartylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String img2Url = "http://i.zeze.com/attachment/forum/201610/30/150453u3oo7n3c702j7f08.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView imageView1 = (ImageView) findViewById(R.id.img1);
-
-
 
     }
 }
